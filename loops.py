5976875001d1bb9c3e0061dfb9d8f0996c851c20
MSmith <marie.ellen@gmail.com>
2025-01-02 13:10:28 -0500

    add additional content loops.py

--- loops.py
+++ loops.py
@@ -7,3 +7,34 @@ while True:
         break
     print(line)
 print('Done!')
+
+
+
+# Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. Once 'done' is entered, print out the largest and smallest of the numbers. If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. Enter 7, 2, bob, 10, and 4 and match the output below.
+
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        num = int(num)
+        if largest is None or num > largest:
+            largest = num
+        if smallest is None or num < smallest:
+            smallest = num
+    except:
+        print("Invalid input")
+   
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
+
+# Exercise 1: Write a while loop that starts at the last character in the string and works its way backwards to the first character in the string, printing each letter on a separate line, except backwards.
+
+string = 'testing'
+count = len(string) - 1
+while count >= 0:
+    print(string[count])
+    count -= 1

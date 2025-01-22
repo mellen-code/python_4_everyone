cb67e0997e9df3a63716e12515d43496aeb5d7f5
MSmith <marie.ellen@gmail.com>
2025-01-22 18:37:15 -0500

    add lists.py exercises and romeo.txt

--- lists.py
+++ lists.py
@@ -149,11 +149,48 @@ print((' ').join(n))
 
 # Exercise 1: Write a function called chop that takes a list and modifies it, removing the first and last elements, and returns None. Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.
 
-def chop(l):
-    l[1:len(l)-1]
+# def chop(l):
+#     l[1:len(l)-1]
 
-print(chop([1,2,3,4]), '- chop function')
+# print(chop([1,2,3,4]), '- chop function')
 
-def middle(l):
-    return l[1:len(l)-1]
-print(middle([5,6,7,8]), '- middle function')
+# def middle(l):
+#     return l[1:len(l)-1]
+# print(middle([5,6,7,8]), '- middle function')
+
+# 8.4
+# Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. When the program completes, sort and print the resulting words in python sort() order as shown in the desired output.
+# You can download the sample data at http://www.py4e.com/code3/romeo.txt
+
+# fname = input('Enter file name: ')
+# fh = open(fname)
+# lst = list()
+# for line in fh:
+#     str_line = line.rstrip()
+#     words = str_line.split()
+#     for word in words:
+#         if word in lst: continue
+#         lst.append(word)
+# print(sorted(lst))
+    # OR: 
+    # lst.sort()
+    # print(lst)
+
+# 8.5
+# Open the file mbox-short.txt and read it line by line. When you find a line that starts with 'From ' like the following line:
+# From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+# You will parse the From line using split() and print out the second word in the line (i.e. the entire address of the person who sent the message). Then print out a count at the end.
+# Hint: make sure not to include the lines that start with 'From:'. Also look at the last line of the sample output to see how to print the count.
+
+# You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
+
+fname = input('Enter file name: ')
+fh = open(fname)
+count = 0
+for line in fh:
+    words = line.split()
+    if len(words) > 1 and words[0] == 'From':
+        print(words[1])
+        count += 1
+        
+print("There were", count, "lines in the file with From as the first word")

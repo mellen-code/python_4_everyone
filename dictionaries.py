b55c029af3ba75dd52c70d074ac2f4debb0ad0ec
MSmith <marie.ellen@gmail.com>
2025-04-01 18:26:55 -0400

    add advanced text parsing logic

--- dictionaries.py
+++ dictionaries.py
@@ -90,3 +90,31 @@ names = ['name1', 'ben', 'cewn', 'cewn']
 # for name in names:
 #     counts[name] = counts.get(name, 0) + 1
 # print(counts)
+
+# ADVANCED TEXT PARSING
+# use translate()
+import string
+
+fname = input('Enter file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened', fname)
+    exit()
+
+counts = dict()
+for line in fhand:
+    line = line.rstrip()
+    
+    line = line.translate(line.maketrans('', '', string.punctuation))
+    line = line.lower()
+    words = line.split()
+
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+
+print(counts)
+

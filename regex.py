834c33f11e7e1af7e364bc64f65f3eff09920c46
MSmith <marie.ellen@gmail.com>
2025-04-10 08:25:17 -0400

    add regex.py

---
+++ regex.py
@@ -0,0 +1,33 @@
+'''Python Regular Expression Quick Guide
+
+^        Matches the beginning of a line
+$        Matches the end of the line
+.        Matches any character
+\s       Matches whitespace
+\S       Matches any non-whitespace character
+*        Repeats a character zero or more times
+*?       Repeats a character zero or more times 
+         (non-greedy)
++        Repeats a character one or more times
++?       Repeats a character one or more times 
+         (non-greedy)
+[aeiou]  Matches a single character in the listed set
+[^XYZ]   Matches a single character not in the listed set
+[a-z0-9] The set of characters can include a range
+(        Indicates where string extraction is to start
+)        Indicates where string extraction is to end
+'''
+
+
+
+# Search for lines that start with From and have an at sign
+import re
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if re.search('^From:.+@', line):
+        print(line)
+
+# This matches lines that start with 'From:', followed by one or more characters (.+), following by an at-sign ---> the last at-sign if there are more than 1 in the line.
+
+# *+  zero or more characters

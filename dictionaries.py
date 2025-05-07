992a899708cabd5250fe7a56ded0d2eebfc74ffb
MSmith <marie.ellen@gmail.com>
2025-05-07 08:07:36 -0400

    add dictionaries.py exercise

--- dictionaries.py
+++ dictionaries.py
@@ -189,23 +189,23 @@ names = ['name1', 'ben', 'cewn', 'cewn']
 
 # Using EXERCISE 3 code, after the loop to add data to dictionary, move dictionary's values to a list, get the max value, and find that value's key in the dictionary and return the pair as a string.
 
-fname = input('Enter file name: ')
-if len(fname) < 1:
-    fname = 'mbox_short.txt'
-    fhand = open(fname)
-else:
-    try:
-        fhand = open(fname)
-    except:
-        print('File not found', fname)
-        exit()
+# fname = input('Enter file name: ')
+# if len(fname) < 1:
+#     fname = 'mbox_short.txt'
+#     fhand = open(fname)
+# else:
+#     try:
+#         fhand = open(fname)
+#     except:
+#         print('File not found', fname)
+#         exit()
 
-emails = dict()
-for line in fhand:
-    words = line.split()
+# emails = dict()
+# for line in fhand:
+#     words = line.split()
 
-    if len(words) > 1 and words[0] == 'From':
-        emails[words[1]] = emails.get(words[1], 0) +1
+#     if len(words) > 1 and words[0] == 'From':
+#         emails[words[1]] = emails.get(words[1], 0) +1
 
 # values = list(emails.values())
 # max_value = max(values)
@@ -213,11 +213,34 @@ for line in fhand:
 # print("".join(key_of_max_value), max_value)
 
 # OR just loop thru the dictionary and store max!
-max_key = ''
+# max_key = ''
 
 # using .get() to search for max_key value in emails. If it doesn't exist, max_key value is set to zero:
-for key in emails:
-    if emails[key] > emails.get(max_key, 0):
-        max_key = key
+# for key in emails:
+#     if emails[key] > emails.get(max_key, 0):
+#         max_key = key
 
-print(max_key, emails[max_key])
+# print(max_key, emails[max_key])
+
+
+# 9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+
+name = input('Enter file name: ')
+if len(name) < 1:
+    name = "mbox_short.txt"
+handle = open(name)
+
+emails = {}
+for line in handle:
+    words = line.split()
+
+    if len(words) > 1 and words[0] == 'From':
+        emails[words[1]] = emails.get(words[1], 0) + 1
+
+max_value = ''
+
+for key in emails:
+    if emails[key] > emails.get(max_value, 0):
+        max_value = key
+
+print(max_value, emails[max_value])

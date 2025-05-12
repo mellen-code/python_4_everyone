b76cf8bb7dc59f3b04e0137836939c393ada78f1
MSmith <marie.ellen@gmail.com>
2025-05-12 07:45:11 -0400

    add tuples.py

---
+++ tuples.py
@@ -0,0 +1,6 @@
+# t = ('a', 'b', 'c', 'd', 'e')
+# You can’t modify the elements of a tuple, but you can replace one tuple with another:
+
+# t = ('A',) + t[1:]
+# print(t)
+# ('A', 'b', 'c', 'd', 'e')

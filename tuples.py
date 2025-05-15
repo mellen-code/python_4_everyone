3ddfe073a870d39170655eab01d5e3d8df29ad23
MSmith <marie.ellen@gmail.com>
2025-05-15 08:05:07 -0400

    add tuples.py tuples assignment

--- tuples.py
+++ tuples.py
@@ -4,3 +4,30 @@
 # t = ('A',) + t[1:]
 # print(t)
 # ('A', 'b', 'c', 'd', 'e')
+
+
+# DSU: Decorate, Sort, Undecorate
+# For example, suppose you have a list of words and you want to sort them from longest to shortest:
+
+txt= 'but soft what light in yonder window breaks'
+words = txt.split()
+t = list()
+for word in words:
+    t.append((len(word), word))
+
+t.sort(reverse=True)
+
+res = list()
+for length,word in t:
+    res.append(word)
+
+print(res)
+
+# TUPLE ASSIGNMENT
+# >>> m = ( 'have', 'fun' )
+# >>> x, y = m
+# >>> x
+# 'have'
+# >>> y
+# 'fun'
+# >>>

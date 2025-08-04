887a602493c9eee1b9c5abd86ae1138a814065b1
MSmith <marie.ellen@gmail.com>
2025-08-04 09:52:38 -0400

    add sockets.py

--- sockets.py
+++ sockets.py
@@ -1,8 +1,15 @@
 import socket
 
+# create socket communication line for this program
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# create connection to server via port 80
 mysock.connect(('data.pr4e.org', 80))
+
+# store get request in variable, encode in bytes
 cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+
+# send get request through connection
 mysock.send(cmd)
 
 while True:
